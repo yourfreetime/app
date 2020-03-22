5d3ee0d18f12c0bbb7ed2f5b1be60afd77bb406f
André Lins <andrelucas01@hotmail.com>
2020-03-22 18:03:11 -0300

    Inserting facebook login

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Image, SafeAreaView } from "react-native";
 import { StackActions } from "@react-navigation/native";
+import { firebase } from "@react-native-firebase/auth";
+import { AccessToken, LoginManager } from "react-native-fbsdk";
+
 import style from "./Login.style";
 
 import Button from "../../components/Button";
@@ -15,7 +18,21 @@ const LoginScreen = ({ navigation }) => (
     <Button
       variant="white"
       title="Login com o Facebook"
-      onPress={() => navigation.dispatch(StackActions.replace("Home"))}
+      onPress={async () => {
+        const result = await LoginManager.logInWithPermissions([
+          "public_profile",
+          "email"
+        ]);
+        console.log(result);
+        const data = await AccessToken.getCurrentAccessToken();
+
+        const credential = firebase.auth.FacebookAuthProvider.credential(
+          data.accessToken
+        );
+
+        const result1 = await firebase.auth().signInWithCredential(credential);
+        console.log(result1);
+      }}
     />
   </SafeAreaView>
 );
