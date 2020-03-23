d9c03cdeeae476affced04f52394b4b704f71e41
André Lins <andrelucas01@hotmail.com>
2020-03-23 19:22:35 -0300

    Inserting buttons in CardPost

--- src/components/CardPost/CardPost.style.js
+++ src/components/CardPost/CardPost.style.js
@@ -5,7 +5,9 @@ const style = StyleSheet.create({
   userImage: { width: 40, height: 40, borderRadius: 100 },
   contentTitle: { justifyContent: "center", marginLeft: 16 },
   userName: { fontSize: 18, fontWeight: "900" },
-  date: { fontSize: 12 }
+  date: { fontSize: 12 },
+  buttons: { flex: 1, flexDirection: "row", alignContent: "stretch" },
+  button: { alignItems: "center", flexDirection: "row" }
 });
 
 export default style;
